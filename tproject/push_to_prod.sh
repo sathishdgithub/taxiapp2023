64111288f2a4efd9c662c4331d36636858352751
adarshash <adarsh@narvar.com>
2019-07-04 09:54:40 -0700

    Remvoing unwanted details

--- tproject/push_to_prod.sh
+++ tproject/push_to_prod.sh
@@ -1,3 +1,11 @@
+#!/usr/bin/env bash
+
+echo "Copying env specific values ..."
+env="prod"
+env_file="~/dev/valv/git/env/taxi/" + $env + "/settings.py"
+cp $env_file tproject/settings.py
+
+# Setup AWS Profile 'valv-ecr' which should have credentials to push to ECR
 echo "switching to valv prod context ..."
 export AWS_PROFILE=valv-ecr
 
@@ -5,7 +13,7 @@ echo "Logging in to ECR ..."
 $(aws ecr get-login --no-include-email --region ap-south-1)
 
 echo "Building Docker Image 'taxiapp' ..."
-docker build -t taxiapp .
+docker build -t taxiapp:latest .
 
 echo "Tagging current build to 'taxiapp:latest' ..."
 docker tag taxiapp:latest 145084937341.dkr.ecr.ap-south-1.amazonaws.com/taxiapp:latest
@@ -13,3 +21,5 @@ docker tag taxiapp:latest 145084937341.dkr.ecr.ap-south-1.amazonaws.com/taxiapp:
 echo "Pushing Image to ECR repository ..."
 docker push 145084937341.dkr.ecr.ap-south-1.amazonaws.com/taxiapp:latest
 
+echo "Restoring local env values ..."
+git checkout -- tproject/settings.py
